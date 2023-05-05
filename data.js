c38732a7127384cca550d7d4271acbaae8cc5886
Luthfi Alfaridz <43901910+xinnxz@users.noreply.github.com>
2023-05-05 15:03:01 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,214 @@
+let products = [{
+        name: "Samsung",
+        desc: "ini Samsung blablablalbalb",
+        harga: "1.400.000",
+        image: "samsung.jpg"
+    },
+    {
+        name: "Xiaomi",
+        desc: "ini Xiaomi blablablalbalb",
+        harga: "1.400.000",
+        image: "xiaomi.jpg"
+    },
+    {
+        name: "IPhone",
+        desc: "ini IPhone blablablalbalb",
+        harga: "1.400.000",
+        image: "i phone.jpg"
+    },
+    {
+        name: "Oppo",
+        desc: "ini Oppo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Vivo",
+        desc: "ini Vivo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Essia",
+        desc: "ini Essia blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Motorola",
+        desc: "ini Motorola blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Samsung",
+        desc: "ini Samsung blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Xiaomi",
+        desc: "ini Xiaomi blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "IPhone",
+        desc: "ini IPhone blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Oppo",
+        desc: "ini Oppo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Vivo",
+        desc: "ini Vivo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Essia",
+        desc: "ini Essia blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Motorola",
+        desc: "ini Motorola blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Samsung",
+        desc: "ini Samsung blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Xiaomi",
+        desc: "ini Xiaomi blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "IPhone",
+        desc: "ini IPhone blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Oppo",
+        desc: "ini Oppo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Vivo",
+        desc: "ini Vivo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Essia",
+        desc: "ini Essia blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Motorola",
+        desc: "ini Motorola blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Samsung",
+        desc: "ini Samsung blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Xiaomi",
+        desc: "ini Xiaomi blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "IPhone",
+        desc: "ini IPhone blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Oppo",
+        desc: "ini Oppo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Vivo",
+        desc: "ini Vivo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Essia",
+        desc: "ini Essia blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Motorola",
+        desc: "ini Motorola blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Samsung",
+        desc: "ini Samsung blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Xiaomi",
+        desc: "ini Xiaomi blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "IPhone",
+        desc: "ini IPhone blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Oppo",
+        desc: "ini Oppo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Vivo",
+        desc: "ini Vivo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Essia",
+        desc: "ini Essia blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Motorola",
+        desc: "ini Motorola blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Samsung",
+        desc: "ini Samsung blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Xiaomi",
+        desc: "ini Xiaomi blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "IPhone",
+        desc: "ini IPhone blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Oppo",
+        desc: "ini Oppo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Vivo",
+        desc: "ini Vivo blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Essia",
+        desc: "ini Essia blablablalbalb",
+        harga: "1.400.000"
+    },
+    {
+        name: "Motorola",
+        desc: "ini Motorola blablablalbalb",
+        harga: "1.400.000"
+    },
+]
